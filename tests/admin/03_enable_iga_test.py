07f85f7999f637e718c07a23147785e88e91d8b2
0xViKi <vijaykrishna19081999@gmail.com>
2025-08-20 17:50:16 +1000

    feature: Added test case enable/disable IGA

---
+++ tests/admin/03_enable_iga_test.py
@@ -0,0 +1,48 @@
+from pytest_bdd import given, when, then, scenarios
+from playwright.sync_api import expect 
+import pytest
+from conftest import take_screenshot
+
+scenarios("admin/enable_iga.feature")
+
+realm_name = "testrealm"
+
+@pytest.mark.dependency(name="enbale_iga", depends=["create_realm"], scope="session")
+@given("the admin is logged in to the Tidecloak admin console")
+def admin_logged_in(logged_in_admin):
+    return logged_in_admin
+
+@when('the admin enables IGA')
+def enable_iga(logged_in_admin):
+    page = logged_in_admin
+    
+    page.get_by_role("link", name="Manage realms").click()
+    page.get_by_role("link", name=realm_name).click()
+
+    page.get_by_role("link", name="Realm settings").click()
+    page.get_by_test_id('rs-general-tab').click()
+
+    page.locator("label[for='tide-realm-iga-switch']").click()
+    page.get_by_role("button", name="Close  alert: IGA changed successfully").click()
+
+
+@then('table in change request for clients is visible')
+def verify_iga(logged_in_admin):
+    page = logged_in_admin
+
+    try:
+        take_screenshot(page, "Enable IGA Successful")
+
+        expect(page.locator("label[for='tide-realm-iga-switch']")).to_be_checked()
+        page.get_by_role("link", name="Change Requests").click()
+        page.get_by_role("tab", name="Clients").click()
+
+        expect(page.get_by_role("button", name="Review Draft")).to_be_visible()
+        expect(page.get_by_role("button", name="Commit Draft")).to_be_visible()
+        expect(page.get_by_role("button", name="Cancel Draft")).to_be_visible()
+        expect(page.get_by_role("grid", name="clientChangeRequestsList")).to_be_visible()
+
+    
+    except:
+        take_screenshot(page, "Enable IGA Failure")
+        raise        
