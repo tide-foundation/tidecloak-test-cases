23aacca11519d8b50bba6ebeecd1021fa49d6719
Tide Foundation <38062419+TideSupreme@users.noreply.github.com>
2025-09-02 16:14:47 +1000

    fix for WSL based distros

--- allure-report.sh
+++ allure-report.sh
@@ -3,31 +3,35 @@
 if [ -n "$VIRTUAL_ENV" ]; then
     echo "[+] Running tests and generating reports..."
     pytest
-    
+
     TEST_EXIT_CODE=$?
     if [ $TEST_EXIT_CODE -eq 0 ]; then
         echo "[+] All tests passed successfully!"
     else
         echo "[+]  Some tests failed, but generating report anyway..."
     fi
-        echo "[+] Starting Allure server..."
+        echo "[+] Starting web server..."
         echo ""
-        echo "[+] Report will open in your browser"
+        echo "[+] Open the reports in your browser at http://127.0.01:3000"
         echo "[+] Press Ctrl+C when you're done viewing the report"
         echo ""
-        
+
         cleanup() {
             echo ""
-            echo "[+] Stopping Allure server..."
-			rm -rf ./reports
+            echo "[+] Stopping web server..."
+            cd ..
+            rm -rf ./reports
+            rm -rf ./allure-report
             echo "[+] Cleanup completed. Goodbye!"
             exit 0
         }
-        
+
         trap cleanup SIGINT SIGTERM
-        
-        allure serve ./reports
-        
-else 
+
+        allure generate ./reports --clean
+        cd allure-report
+        command -v python3 >/dev/null && python3 -m http.server 3000 || python -m http.server 3000
+
+else
     echo "[+] Virtual environment not active..." && exit 1
 fi
